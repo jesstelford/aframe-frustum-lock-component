8b618a75ccdcf5e3de0587c2049261ae82e42b03
Jess Telford <jess+github@jes.st>
2016-10-14 21:55:20 +1100

    Updates

---
+++ main.js
@@ -0,0 +1,6 @@
+import aframe from 'aframe';
+import registerVideoBillboard from 'aframe-video-billboard';
+import registerFrustumLockComponent from '../src/index';
+
+registerVideoBillboard(aframe);
+registerFrustumLockComponent(aframe);
